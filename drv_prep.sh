5aeb3711870f7f771bd446817d66b43038a60f03
yeepeekai <yeepeekai@gmail.com>
2026-09-24 01:46:45 +0200

    Add repository source to script header

--- drv_prep.sh
+++ drv_prep.sh
@@ -2,6 +2,7 @@
 
 # Copyright (c) 2026 Yeepee
 # SPDX-License-Identifier: MIT
+# Source: https://github.com/Yeepee/dvr_prep
 
 # Fixes AAC audio for DaVinci Resolve on Linux by remuxing MP4 (H.264/H.265) 
 # into MOV with 24-bit PCM audio without re-encoding video.
